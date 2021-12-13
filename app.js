d6c46bf43dbf1a1c895c01d43d9ec7c54efd3d16
wenrou77 <wendyog@gmail.com>
2021-12-13 20:15:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+mongoose.connect('mongodb://localhost/short-url', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello')
